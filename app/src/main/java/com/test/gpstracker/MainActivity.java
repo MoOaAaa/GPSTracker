7fbac88f7b5a2a7abe410c4083c62e545f53e398
MoOaAaa <lucas.heidet@orange.fr>
2022-07-24 08:12:15 +0200

    first location update

--- app/src/main/java/com/test/gpstracker/MainActivity.java
+++ app/src/main/java/com/test/gpstracker/MainActivity.java
@@ -1,14 +1,171 @@
 package com.test.gpstracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int BASE_SEC_INTERVAL = 5;
+    public static final int FASTEST_SEC_INTERVAL = 1;
+    private static final int PERMISSIONS_FINE_LOCATION = 9;
+    //UI elements
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates;
+
+    Switch sw_locationUpdates, sw_gps;
+
+    LocationRequest locationRequest;
+
+    LocationCallback locationCallBack;
+
+    //Google's API for locations services
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates = findViewById(R.id.tv_updates);
+        sw_locationUpdates = findViewById(R.id.sw_locationsupdates);
+        sw_gps = findViewById(R.id.sw_gps);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000 * BASE_SEC_INTERVAL);
+        locationRequest.setFastestInterval(1000 * FASTEST_SEC_INTERVAL);
+        locationRequest.setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Location location = locationResult.getLastLocation();
+                updateUIValues(location);
+            }
+        };
+
+        sw_gps.setOnClickListener(view -> {
+            if (sw_gps.isChecked()) {
+                locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+                tv_sensor.setText("Using GPS sensors");
+            } else {
+                locationRequest.setPriority(Priority.PRIORITY_BALANCED_POWER_ACCURACY);
+                tv_sensor.setText("Using Towers and WiFi");
+            }
+        });
+
+        sw_locationUpdates.setOnClickListener(view -> {
+            if (sw_locationUpdates.isChecked()) {
+                startLocationUpdate();
+            } else {
+                stopLocationUpdate();
+            }
+        });
+
+        updateGPS();
+    }
+
+    private void stopLocationUpdate() {
+        tv_updates.setText("Location is not being tracked");
+        tv_lat.setText("Not tracking");
+        tv_lon.setText("Not tracking");
+        tv_altitude.setText("Not tracking");
+        tv_accuracy.setText("Not tracking");
+        tv_speed.setText("Not tracking");
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
+    }
+
+    private void startLocationUpdate() {
+        tv_updates.setText("Location is being tracked");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+        }else{
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+        }
+        updateGPS();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "This app requires permission to work properly", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    private void updateGPS(){
+        //get permission from user
+        //get current location
+        //update UI
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        //check if we have permissions
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    updateUIValues(location);
+
+                }
+            });
+        }
+        //permissions not granted
+        else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void updateUIValues(Location location) {
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+
+        if (location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        }else{
+            tv_altitude.setText("Not available");
+        }
+
+        if (location.hasSpeed()){
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        }else{
+            tv_speed.setText("Not available");
+        }
     }
 }
